3d24e75cddabb8acbbcbad849e560a631a49f74b
Tori Lew <torirlew@gmail.com>
2023-04-27 15:28:14 -0700

    Toggles, Steppers, and Sliders

--- Chapter09/Exercise.swift
+++ Chapter09/Exercise.swift
@@ -6,27 +6,41 @@ import SwiftUI
 struct Exercise: View {
     @State var myToggle = true
     @State var sliderValue = 20.0
-    
+    @State var color: Color = .white
     var body: some View {
         VStack {
             ZStack {
                 Rectangle()
-                    .frame(width: 200, height: 300)
+                    .frame(width: 10*sliderValue, height: 15*sliderValue)
+                    .foregroundColor(myToggle ? color : .mint)
                 Image("Green-Frame")
                     .resizable()
-                    .frame(width: 210, height: 330)
+                    .frame(width: 10.5*sliderValue, height: 16.5*sliderValue)
             }
                 .frame(width:200, height: 300)
+                .padding()
             
+            Toggle(myToggle ? "Custom" : "Mint", isOn: $myToggle)
+                .frame(width: 250)
+                .padding()
+
             VStack {
-                Text("PUT THE STEPPER HERE")
+                Stepper(value: $sliderValue, in: 1...20) {
+                                Text("Stepper value = \(sliderValue)")
+                    
+                            }.padding()
             }.padding()
             
             VStack {
-                Text("PUT THE SLIDER HERE")
+                
+                Text("Slider value = \(sliderValue)")
+                Slider(value: $sliderValue, in: 1...20)
                 Text("Adjust the size of the image")
                 Spacer()
             }
+        }
+        .onChange(of: sliderValue/20) { newValue in
+            color = Color(red: newValue, green: 1 - newValue, blue: 0 + sliderValue)
         }
         .padding()
     }
